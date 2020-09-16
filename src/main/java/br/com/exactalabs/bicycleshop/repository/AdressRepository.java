dd45326db12d6385929cb1a5af651f87a6ecaaa9
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-09-16 16:49:07 -0300

    Creating Client Service

---
+++ src/main/java/br/com/exactalabs/bicycleshop/repository/AdressRepository.java
@@ -0,0 +1,13 @@
+package br.com.exactalabs.bicycleshop.repository;
+
+import br.com.exactalabs.bicycleshop.entity.Adress;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdressRepository extends JpaRepository<Adress, Long> {
+
+
+
+
+}
