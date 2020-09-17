a89128ff60076afda3250ecfc723cf7ad3163f85
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-09-17 11:24:58 -0300

    Creating ORM, get and set ProductCategory class

--- src/main/java/br/com/exactalabs/bicycleshop/entity/ProductCategory.java
+++ src/main/java/br/com/exactalabs/bicycleshop/entity/ProductCategory.java
@@ -1,22 +1,58 @@
 package br.com.exactalabs.bicycleshop.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
+
+@Entity
+@Table(name = "productCategory")
 public class ProductCategory {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull(message = "O nome não pode ser nulo")
+    @NotBlank(message = "O nome não pode ser vazio")
     private String name;
+    @NotNull(message = "A categoria não pode ser nulo")
+    @OneToMany(mappedBy = "productCategory")
+    private Collection<Product> productList;
 
-    public ProductCategory(){
-
-    }
-
-    public ProductCategory(String name){
+    public ProductCategory(String name, Collection<Product> productList) {
         this.name = name;
+        this.productList = productList;
     }
 
-    public String getName(){
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Collection<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(Collection<Product> productList) {
+        this.productList = productList;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
