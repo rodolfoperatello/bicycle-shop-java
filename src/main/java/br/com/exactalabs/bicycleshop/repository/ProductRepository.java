30111c0312ee9796d12463a9685d8b9448aeab26
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-09-17 14:25:47 -0300

    Add methods findByPrice, findByName and findByCategory for Product

--- src/main/java/br/com/exactalabs/bicycleshop/repository/ProductRepository.java
+++ src/main/java/br/com/exactalabs/bicycleshop/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package br.com.exactalabs.bicycleshop.repository;
 
 import br.com.exactalabs.bicycleshop.entity.Product;
-import org.hibernate.annotations.NamedNativeQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,18 +8,18 @@ import java.util.Collection;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query(value = "SELECT t from Product t order by price")
+    @Query(value = "SELECT t FROM Product t ORDER BY price")
     Collection<Product> findProductByPriceAsc();
 
-    @Query(value = "SELECT t from Product t order by price desc")
+    @Query(value = "SELECT t FROM Product t ORDER BY price DESC")
     Collection<Product> findProductByPriceDesc();
 
-    @Query(value = "SELECT t from Product t where name like %?1% order by name ASC")
+    @Query(value = "SELECT t FROM Product t WHERE name like %?1% ORDER BY name ASC")
     Collection<Product> findProductByName(String productName);
 
 
-//    @Query(value = "SELECT t from Product t left join productCategory p on t.productCategory = p.id where p.name like %?1%")
-//    Collection<Product> findProductByCategory(String categoryName);
+    @Query(value = "SELECT t from Product t LEFT JOIN ProductCategory p ON t.productCategory = p.id WHERE p.name LIKE %?1% ORDER BY t.name ASC")
+    Collection<Product> findProductByCategory(String categoryName);
 
 
 
