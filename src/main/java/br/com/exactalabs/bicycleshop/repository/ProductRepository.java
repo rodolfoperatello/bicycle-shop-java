62a325e22320b87f29c97b63415aa5ba33d0a635
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-09-17 18:19:16 -0300

    Removing Queries with JPQL, creating update, pageRequest method and add Pageable for ProductService methods

--- src/main/java/br/com/exactalabs/bicycleshop/repository/ProductRepository.java
+++ src/main/java/br/com/exactalabs/bicycleshop/repository/ProductRepository.java
@@ -1,25 +1,23 @@
 package br.com.exactalabs.bicycleshop.repository;
 
 import br.com.exactalabs.bicycleshop.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.Collection;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query(value = "SELECT t FROM Product t ORDER BY price")
-    Collection<Product> findProductByPriceAsc();
+    Page<Product> findAllProductByNameLikeOrderByNameAsc(String name, Pageable pageRequest);
 
-    @Query(value = "SELECT t FROM Product t ORDER BY price DESC")
-    Collection<Product> findProductByPriceDesc();
+    Page<Product> findAllProductByProductCategoryNameOrderByNameAsc(String name, Pageable pageRequest);
 
-    @Query(value = "SELECT t FROM Product t WHERE name like %?1% ORDER BY name ASC")
-    Collection<Product> findProductByName(String productName);
+    Page<Product> findAllProductByOrderByPriceAsc(Pageable pageRequest);
+
+    Page<Product> findAllProductByOrderByPriceDesc(Pageable pageRequest);
 
 
-    @Query(value = "SELECT t from Product t LEFT JOIN ProductCategory p ON t.productCategory = p.id WHERE p.name LIKE %?1% ORDER BY t.name ASC")
-    Collection<Product> findProductByCategory(String categoryName);
+
 
 
 
--- src/main/java/br/com/exactalabs/bicycleshop/service/ProductService.java
+++ src/main/java/br/com/exactalabs/bicycleshop/service/ProductService.java
@@ -4,8 +4,11 @@ package br.com.exactalabs.bicycleshop.service;
 import br.com.exactalabs.bicycleshop.entity.Product;
 import br.com.exactalabs.bicycleshop.repository.ProductCategoryRepository;
 import br.com.exactalabs.bicycleshop.repository.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Collection;
 
 @Service
@@ -19,6 +22,12 @@ public class ProductService {
         this.productCategoryRepository = productCategoryRepository;
     }
 
+    private PageRequest createPageRequest(Integer pageNumber, Integer pageSize){
+        var pageRequest = PageRequest.of(pageNumber, pageSize);
+        return pageRequest;
+    }
+
+    @Transactional
     public Product saveProduct(Product product) {
         this.productCategoryRepository.save(product.getProductCategory());
         return this.productRepository.save(product);
@@ -28,21 +37,45 @@ public class ProductService {
         this.productRepository.deleteById(id);
     }
 
-    public Collection<Product> findProductByPriceAsc() {
-        return this.productRepository.findProductByPriceAsc();
+    public Product updateProduct(Product product){
+        return this.productRepository.save(product);
     }
 
-    public Collection<Product> findProductByPriceDesc() {
-        return this.productRepository.findProductByPriceDesc();
+    public Product findProductById(Long id){
+        return this.productRepository.findById(id).orElseThrow(() -> new RuntimeException("Produto não encontrado"));
     }
 
-    public Collection<Product> findProductByName(String name){
-        return this.productRepository.findProductByName(name);
+    public Collection<Product> findAllProducts(){
+        return this.productRepository.findAll();
     }
 
-    public Collection<Product> findProductByCategory(String name){
-        return this.productRepository.findProductByCategory(name);
+    public Page<Product> findAllProductByName(String name, Integer pageNumber){
+        var pageRequest = createPageRequest(pageNumber, 30);
+        return this.productRepository.findAllProductByNameLikeOrderByNameAsc(name, pageRequest);
     }
+
+    public Page<Product> findAllProductsByCategory(String name, Integer pageNumber) {
+        var pageRequest = createPageRequest(pageNumber, 30);
+        return this.productRepository.findAllProductByProductCategoryNameOrderByNameAsc(name, pageRequest);
+    }
+
+    public Page<Product> findAllProductByPriceAsc(Integer pageNumber) {
+        var pageRequest = createPageRequest(pageNumber, 30);
+        return this.productRepository.findAllProductByOrderByPriceAsc(pageRequest);
+    }
+
+    public Page<Product> findAllProductByPriceDesc(Integer pageNumber) {
+        var pageRequest = createPageRequest(pageNumber, 30);
+        return this.productRepository.findAllProductByOrderByPriceDesc(pageRequest);
+    }
+
+
+//    public Collection<Product> findProductsByName(String name){
+//        return this.productRepository.findProductByName(name);
+//    }
+
+
+
 
 
 
